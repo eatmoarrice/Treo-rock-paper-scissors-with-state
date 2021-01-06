8e9496149fee448ffc80ffdfbfbf21ac92e54d81
unknown <xvinh.ir@gmail.com>
2021-01-06 11:31:26 +0700

    added rockets

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Button, ButtonGroup } from 'react-bootstrap';
-import './App.css';
-import ChoiceCard from './components/ChoiceCard';
-import PublicNavbar from './components/PublicNavbar';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Button, ButtonGroup } from "react-bootstrap";
+import "./App.css";
+import ChoiceCard from "./components/ChoiceCard";
+import PublicNavbar from "./components/PublicNavbar";
 
 function App() {
-	const shapes = ['rock', 'paper', 'scissors'];
-	const [playerChoice, setPlayerChoice] = useState('paper');
-	const [playerResult, setPlayerResult] = useState('tie');
+	const shapes = ["rock", "paper", "scissors"];
+	const [playerName, setPlayerName] = useState("You");
+	const [playerChoice, setPlayerChoice] = useState("");
+	const [playerResult, setPlayerResult] = useState("tie");
 	const [playerScore, setPlayerScore] = useState(0);
-	const [computerChoice, setComputerChoice] = useState('paper');
-	const [computerResult, setComputerResult] = useState('tie');
+	const [computerChoice, setComputerChoice] = useState("");
+	const [computerResult, setComputerResult] = useState("tie");
 	const [computerScore, setComputerScore] = useState(0);
 
 	const randomMove = (move) => {
@@ -24,58 +25,79 @@ function App() {
 
 	const calculateWinner = (computerChoice, playerChoice) => {
 		if (computerChoice === playerChoice) {
-			setComputerResult('tie');
-			setPlayerResult('tie');
-		} else if (computerChoice === 'rock') {
-			if (playerChoice === 'paper') {
-				setComputerResult('loss');
-				setPlayerResult('win');
+			setComputerResult("tie");
+			setPlayerResult("tie");
+		} else if (computerChoice === "rock") {
+			if (playerChoice === "paper") {
+				setComputerResult("loss");
+				setPlayerResult("win");
 				setPlayerScore(playerScore + 1);
 			} else {
-				setComputerResult('win');
+				setComputerResult("win");
 				setComputerScore(computerScore + 1);
-				setPlayerResult('loss');
+				setPlayerResult("loss");
 			}
-		} else if (computerChoice === 'paper') {
-			if (playerChoice === 'scissors') {
-				setComputerResult('loss');
-				setPlayerResult('win');
+		} else if (computerChoice === "paper") {
+			if (playerChoice === "scissors") {
+				setComputerResult("loss");
+				setPlayerResult("win");
 				setPlayerScore(playerScore + 1);
 			} else {
-				setComputerResult('win');
+				setComputerResult("win");
 				setComputerScore(computerScore + 1);
-				setPlayerResult('loss');
+				setPlayerResult("loss");
 			}
 		} else {
-			if (playerChoice === 'rock') {
-				setComputerResult('loss');
-				setPlayerResult('win');
+			if (playerChoice === "rock") {
+				setComputerResult("loss");
+				setPlayerResult("win");
 				setPlayerScore(playerScore + 1);
 			} else {
-				setComputerResult('win');
+				setComputerResult("win");
 				setComputerScore(computerScore + 1);
-				setPlayerResult('loss');
+				setPlayerResult("loss");
 			}
 		}
 	};
 
+	const restart = () => {
+		setPlayerChoice("");
+		setPlayerResult("tie");
+		setPlayerScore(0);
+		setComputerChoice("");
+		setComputerResult("tie");
+		setComputerScore(0);
+	};
+
+	const handleChange = (e) => {
+		if (e.target.value) {
+			setPlayerName(e.target.value);
+		} else {
+			setPlayerName("You");
+		}
+	};
 	return (
 		<div className="App text-center">
 			<PublicNavbar />
 			<Container>
+				Name:
+				<input class="m-2" onChange={(e) => handleChange(e)}></input>
 				<div className="d-flex justify-content-center flex-wrap">
-					<ChoiceCard title="You" result={playerResult} shape={playerChoice} score={playerScore} />
+					<ChoiceCard title={playerName} result={playerResult} shape={playerChoice} score={playerScore} />
 					<ChoiceCard title="Computer" result={computerResult} shape={computerChoice} score={computerScore} />
 				</div>
 				<ButtonGroup>
-					<Button variant="outline-dark" className="mx-1" onClick={() => randomMove('rock')}>
+					<Button variant="outline-dark" className="mx-1" onClick={() => randomMove("rock")}>
 						Play 👊
 					</Button>
-					<Button variant="outline-dark" className="mx-1" onClick={() => randomMove('paper')}>
+					<Button variant="outline-dark" className="mx-1" onClick={() => randomMove("paper")}>
 						Play 🤚
 					</Button>
-					<Button variant="outline-dark" className="mx-1" onClick={() => randomMove('scissors')}>
+					<Button variant="outline-dark" className="mx-1" onClick={() => randomMove("scissors")}>
 						Play ✌
+					</Button>
+					<Button variant="dark" onClick={restart}>
+						RESTART
 					</Button>
 				</ButtonGroup>
 			</Container>
--- src/components/ChoiceCard.js
+++ src/components/ChoiceCard.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import rock from '../images/rock.png';
-import paper from '../images/paper.png';
-import scissors from '../images/scissors.png';
+import React from "react";
+import rock from "../images/rock.png";
+import paper from "../images/paper.png";
+import scissors from "../images/scissors.png";
+import question from "../images/question.png";
 
 const ChoiceCard = (props) => {
 	return (
-		<div className={`choice-card ${props.result === 'win' ? 'border-success' : props.result === 'tie' ? 'border-dark' : 'border-danger'}`}>
+		<div className={`choice-card ${props.result === "win" ? "border-success" : props.result === "tie" ? "border-dark" : "border-danger"}`}>
 			<h2>{props.title}</h2>
-			<img src={props.shape === 'rock' ? rock : props.shape === 'paper' ? paper : scissors} alt={props.shape} />
-			<h3>{props.result === 'win' ? 'WIN' : props.result === 'tie' ? 'TIE' : 'LOSS'}</h3>
+			<img
+				src={props.shape === "scissors" ? scissors : props.shape === "rock" ? rock : props.shape === "paper" ? paper : question}
+				alt={props.shape}
+			/>
+			<h3>{props.result === "win" ? "WIN" : props.result === "tie" ? "TIE" : "LOSS"}</h3>
 			<h4>{props.score}</h4>
 		</div>
 	);
